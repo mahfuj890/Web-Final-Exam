b348c3a35b94c24b5e89f3ef6289cf435095ad38
mahfuj890 <mahfujali490@gmail.com>
2022-06-25 12:41:19 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,59 @@
+
+
+<?php  ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web Lab Final</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+
+</head>
+
+<body>
+
+    <form action="" class="form_area" id="form">
+        <div class="table-responsive">
+
+
+        <table class="table table-bordered table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Age</th>
+                    <th scope="col">User Name</th>
+                    <th scope="col">Password</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    <td>@mdo</td>
+                </tr>
+
+            </tbody>
+        </table>
+        </div>
+
+
+    </form>
+
+
+
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script
+      src="https://kit.fontawesome.com/46f35fbc02.js"
+      crossorigin="anonymous"
+    ></script>
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
